1c4faeed65de26d66304b66cbd011088fa2eae73
Nicoly Malescza <Nicolysilvamalescza@gmail.com>
2023-06-12 08:33:29 -0300

    Mini Calculadora

---
+++ ListaAVA-1Matemática/Matematica.cs
@@ -0,0 +1,40 @@
+﻿
+class Matematica
+{
+    public Matematica()
+    {    
+    }
+    public double Somar(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double Maior(double a, double b)
+    {
+        if (a > b)
+        {
+            Console.WriteLine($"{a} e o maior número");
+            return a;
+        }
+
+        else if (a < b)
+        {
+            Console.WriteLine($"{b} e o maior número");
+            return b;
+        }
+        else
+            return a;
+    }
+
+    public double RaizQuadrada(double a)
+    {
+        if(a > 0)
+        {
+            return Math.Sqrt(a);
+        }
+        else
+            Console.WriteLine("Impossivel calcular!");
+        
+        return 0;
+    }
+}
---
+++ ListaAVA-1Matemática/Program.cs
@@ -0,0 +1,38 @@
+﻿Matematica m = new Matematica();
+erro:;
+while (true)
+{
+    Console.WriteLine("\n0-Sair\n1-Somar\n2-Maior número\n3-Raiz quadrada\nescolha o que deseja fazer");
+    byte opcao = Convert.ToByte(Console.ReadLine());
+
+
+    switch (opcao)
+    {
+        case 0:
+            goto sair;
+
+        case 1:
+            Console.WriteLine("Digite dois valores pra ser feita a soma");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Resultado da soma: {m.Somar(num1, num2)}");
+            break;
+
+        case 2:
+            Console.WriteLine("Digite dois valores para verificação do maior número");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Resultado: {m.Maior(num1, num2)}");
+            break;
+
+        case 3:
+            Console.WriteLine("Digite o valor para verificar a raiz quadrada");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Raiz: {m.RaizQuadrada(num1)}");
+            break;
+
+        default:
+            goto erro;
+    }
+}
+sair:;
